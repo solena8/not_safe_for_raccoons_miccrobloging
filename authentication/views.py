001dcf66a03b0ee48843c9f3e04757462958fd47
solena8 <solena_ananda@hotmail.com>
2024-12-04 10:39:21 +0100

    fix: login via classe intégrée de django

--- authentication/views.py
+++ authentication/views.py
@@ -1,26 +1,7 @@
 from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect
 
-from . import forms
-
 
 def logout_user(request):
     logout(request)
     return redirect('login')
-
-
-def login_page(request):
-    form = forms.LoginForm()
-    message = ''
-    if request.method == 'POST':
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            user = authenticate(
-                username=form.cleaned_data['username'],
-                password=form.cleaned_data['password'],
-            )
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-        message = 'Identifiants invalides.'
-    return render(request, 'authentication/login.html', context={'form': form, 'message': message})
--- nsfr/settings.py
+++ nsfr/settings.py
@@ -123,3 +123,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authentication.User'
 
 LOGIN_URL = 'login'
+
+LOGIN_REDIRECT_URL = 'home'
