60df3be2d3d24fc373c08dd216294c3166ee77d8
solena8 <solena_ananda@hotmail.com>
2024-12-05 16:23:09 +0100

    fix-using fictures & conftest.py

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from authentication.models import User
+
+
+@pytest.fixture()
+def test_server(page, live_server):
+    page.goto(live_server.url)
+    return page
+
+
+@pytest.fixture
+def test_user(db):
+    return User.objects.create_user(
+        username="test_user2",
+        password="secure_password"
+    )
+
--- tests-E2E/test_login.py
+++ tests-E2E/test_login.py
@@ -1,31 +1,25 @@
-
 import re
+import os
+os.environ['DJANGO_ALLOW_ASYNC_UNSAFE'] = '1'
 from playwright.sync_api import Page, expect
 
-base_url = "http://127.0.0.1:8000/"
 
-def test_has_title(page: Page):
-    page.goto(base_url)
-
+def test_has_title(page: Page, test_server):
     expect(page).to_have_title(re.compile("Not Safe For Raccoons"))
 
 
-def test_connect_button_is_visible(page: Page):
-    page.goto(base_url)
+def test_connect_button_is_visible(page: Page, test_server):
     login_button = page.get_by_test_id("login_button")
     expect(login_button).to_be_visible()
 
 
-def test_connect_username_form_is_visible(page: Page):
-    page.goto(base_url)
+def test_connect_username_form_is_visible(page: Page, test_server):
     login_form = page.get_by_test_id("login_form")
     expect(login_form).to_be_visible()
 
-def test_connection_works_and_redirects_home(page: Page):
-    page.goto(base_url)
-    username_label = page.get_by_label("Nom d’utilisateur")
-    username_label.fill("test_1")
-    password_label = page.get_by_label("Mot de passe")
-    password_label.fill("test")
+def test_connection_works_and_redirects_home(page: Page, test_user, live_server, test_server):
+    page.get_by_label("Nom d’utilisateur").fill(test_user.username)
+    page.get_by_label("Mot de passe").fill("secure_password")
     page.get_by_role("button", name="Se connecter").click()
-    assert page.url == base_url + "home/"
+
+    assert page.url == live_server.url + "/home/"
