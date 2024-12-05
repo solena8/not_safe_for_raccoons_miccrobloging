14478f1f42473fb62e03ccc24d40a67c8e2cfda4
Fiona <fiona.rio@outlook.com>
2024-12-05 14:07:16 +0100

    ajout d'un formulaire de creation de posts, les posts sont visibles

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,31 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
+from blog import forms, models
 
 
 @login_required
 def home(request):
-    return render(request, 'blog/home.html')
+    blogs = models.Blog.objects.all()
+    return render(request, 'blog/home.html', context={'blogs': blogs})
+
+@login_required
+def blog_upload(request):
+    blog_form = forms.BlogForm()
+    if request.method == 'POST':
+        blog_form = forms.BlogForm(request.POST)
+        if blog_form.is_valid():
+            blog = blog_form.save(commit=False)
+            blog.author = request.user
+            blog.save()
+            return redirect('home')
+    context = {
+        'blog_form': blog_form,
+}
+    return render(request, 'blog/create_blog_post.html', context=context)
+
+
+@login_required
+def view_blog(request, blog_id):
+    blog = get_object_or_404(models.Blog, id=blog_id)
+    return render(request, 'blog/view_blog.html', {'blog': blog})
