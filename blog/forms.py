ed653f37bff0d370366a34fd3ff620f0c1a7d705
Fiona <fiona.rio@outlook.com>
2024-12-09 16:16:22 +0100

    ajout des images et css moche

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from . import models
 
+class PhotoForm(forms.ModelForm):
+    class Meta:
+        model = models.Photo
+        fields = ['image', 'caption']
+
 class BlogForm(forms.ModelForm):
     class Meta:
         model = models.Blog
