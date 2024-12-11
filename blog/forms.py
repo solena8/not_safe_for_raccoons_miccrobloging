77a4425fce9993a9ccb47c73779470ec2e633b47
Fiona <fiona.rio@outlook.com>
2024-12-11 17:37:08 +0100

    css ok je crois

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,7 @@ from . import models
 class PhotoForm(forms.ModelForm):
     class Meta:
         model = models.Photo
-        fields = ['image', 'caption']
+        fields = ['image']
 
 class BlogForm(forms.ModelForm):
     class Meta:
